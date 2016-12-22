0c0a822ce82a422a53e92575c30d4c0ae1164e43
illad <varunvasudevan11@gmail.com>
2016-12-22 22:47:12 +0530

    First commit

---
+++ login/models.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    
+    ###status of the advertisement	
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Access(models.Model):
+	name=models.CharField(max_length=200)
+	phone=models.CharField(max_length=200)
+	ip=models.CharField(max_length=200)
+	mac=models.CharField(max_length=200)
+	OTP=models.CharField(max_length=4)
+	
+	def __self__(self):
+		return self.name
+	
